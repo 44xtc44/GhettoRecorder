e3496ba483bdf8cf8787a6d25aa24c1e0c961d6a
4xtc <44xtc44@gmail.com>
2022-11-07 11:25:15 +0100

    writes path to api now

--- ghettorecorder/ghetto_container.py
+++ ghettorecorder/ghetto_container.py
@@ -1,21 +1,21 @@
-""" module for ghettorecorder Python package to container deployment
+""" module for ghettorecorder Python package to container deployment (read only, or restore on shutdown fs)
 
 Target
    docker container
    snap container, snapcraft setup for linux OS
 
 Functions
-   container_setup_use()  - decide to set up a container env
-   container_config_dir() - get path for new folder creation
-   create_config_env()    - overwrite base dir for ghetto, copy config file to that dir
+   container_setup  - decide to set up a container env
+   container_config_dir - get path for new folder creation
+   create_config_env    - overwrite base dir for ghetto, copy config file to that dir
 """
 import os
 import shutil
 import getpass
-import ghettorecorder.ghetto_ini as ghetto_ini
+from ghettorecorder.api import ghettoApi
 
 
-def container_setup_use():
+def container_setup():
     """ return False if no package specific env variable is set
 
      Info
@@ -24,16 +24,16 @@ def container_setup_use():
         copy settings.ini to that path
     """
     is_container = False
-    is_snap_package = 'GHETTORECORDER_SNAP' in os.environ   # var must be set in package config
-    is_docker_device = 'DOCKER' in os.environ               # var must be set in package config
+    is_snap_container = 'GHETTORECORDER_SNAP' in os.environ   # var must be set in package config, some value
+    is_docker_container = 'DOCKER' in os.environ              # var must be set in package config, some value
 
-    if is_snap_package:
+    if is_snap_container:
         print('Snap Container')
         is_container = True
         container_config_dir('SNAP')
         return is_container
 
-    if is_docker_device:
+    if is_docker_container:
         print('Docker Container')
         is_container = True
         container_config_dir('DOCKER')
@@ -66,8 +66,7 @@ def create_config_env(ghetto_folder):
        copy settings.ini to that folder, blacklist is created automatically if choice
     """
     make_config_folder(ghetto_folder)
-    ghetto_ini.GIni.global_custom_path = ghetto_folder   # global seems bad idea, only one attribut change, settings_dir
-    ghetto_ini.GIni.settings_dir = ghetto_folder         # todo possible bug in blacklist creation functions
+    ghettoApi.config_dir = ghetto_folder
     source_ini = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.ini')
     dst_ini = os.path.join(ghetto_folder, 'settings.ini')
     container_copy_settings(source_ini, dst_ini)
