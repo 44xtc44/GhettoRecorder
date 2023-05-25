a24ef73d5f471df4c0e57e8ff8fa503177b94f7f
4xtc <44xtc44@gmail.com>
2023-05-25 21:49:19 +0200

    fix container env var check

--- ghettorecorder/ghetto_container.py
+++ ghettorecorder/ghetto_container.py
@@ -34,21 +34,39 @@ def container_setup():
     :returns: True if container
     """
     is_container = False
-    is_snap_container = 'GHETTORECORDER_SNAP' in os.environ   # var must be set in package config, some value
-    is_docker_container = 'DOCKER' in os.environ              # var must be set in package config, some value
+    is_snap = 'SNAP' in os.environ
+    is_docker = 'DOCKER' in os.environ
 
-    if is_snap_container:
-        print('Snap Container')
+    if is_snap:
+        get_env_snap()
         is_container = True
         container_config_dir('SNAP')
         return is_container
 
-    if is_docker_container:
-        print('Docker Container')
+    if is_docker:
+        get_env_docker()
         is_container = True
         container_config_dir('DOCKER')
         return is_container
     return is_container
+
+
+def get_env_snap():
+    print('GhettoRecorder App in Snap Container, check environment:\n')
+    print('SNAP_USER_COMMON: ' + os.environ["SNAP_USER_COMMON"])
+    print('SNAP_LIBRARY_PATH: ' + os.environ["SNAP_LIBRARY_PATH"])
+    print('SNAP_COMMON: ' + os.environ["SNAP_COMMON"])
+    print('SNAP_USER_DATA: ' + os.environ["SNAP_USER_DATA"])
+    print('SNAP_DATA: ' + os.environ["SNAP_DATA"])
+    print('SNAP_REVISION: ' + os.environ["SNAP_REVISION"])
+    print('SNAP_NAME: ' + os.environ["SNAP_NAME"])
+    print('SNAP_ARCH: ' + os.environ["SNAP_ARCH"])
+    print('SNAP_VERSION: ' + os.environ["SNAP_VERSION"])
+    print('SNAP: ' + os.environ["SNAP"])
+
+
+def get_env_docker():
+    print('\n\tGhettoRecorder App in Docker Container\n')
 
 
 def container_config_dir(container):
