a6b4b57c9c25bf310ed7457cce277e3490337913
4xtc <44xtc44@gmail.com>
2023-09-12 17:23:27 +0200

    each instance has info_dump_dct to upd Ghetto API via eval

--- ghettorecorder/__init__.py
+++ ghettorecorder/__init__.py
@@ -80,7 +80,8 @@ class GhettoRecorder(threading.Thread):
         # com ports, creator of instance attach queues
         self.com_in = None  # rv_tup = (radio, [str 'eval' or 'exec'], str 'command')
         self.com_out = None  # result
-        self.audio_out = None  # grab response chunks; web server can loop through to a browser
+        self.audio_out = None  # grab response chunks; web server can offer them JS audio elem on endpoint
+        self.info_dump_dct = {}  # eval reads dict and caller puts useful info in Ghetto API
 
         # inbuilt features, switched from other modules
         self.runs_meta = False  # call metadata periodically, create path for rec out; False: recorder is the file
@@ -337,12 +338,12 @@ class GhettoRecorder(threading.Thread):
                 self.meta_data_inst.meta_get(self.radio_url, self.radio_name,
                                              self.radio_dir,
                                              self.suffix, self.bit_rate, self.user_agent)
+                self.info_dump_dct = self.meta_data_inst.header_info_dict
                 self.new_title = self.meta_data_inst.title
                 self.new_dst = self.meta_data_inst.title_path
 
                 if self.new_title:
                     self.metadata_title_change()
-                    # ghettoApi.info.current_title_dict[self.radio_name] = self.new_title
 
             for sec in range(4):  # 4 x .5 sec
                 time.sleep(.5)
